cf3c36e8159f05ff26ff5066b71e74b730133969
Vanderley Quaresma <1012331@sal.ipg.pt>
2018-10-19 11:32:31 +0100

    Implementação de Controladores e Modelos dos Enfermeiros

---
+++ PI_ES2_Grupo8/Controllers/EnfermeirosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PI_ES2_Grupo8.Models;
+
+namespace PI_ES2_Grupo8.Controllers
+{
+    public class EnfermeirosController : Controller
+    {
+        public IActionResult ViEnfermeiros()
+        {
+            return View();
+        }
+
+        public IActionResult ViEnfermeiros(Enfermeiros enfermeiro)
+        {
+    
+            return View();
+        }
+    }
+}
---
+++ PI_ES2_Grupo8/Models/Enfermeiros.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PI_ES2_Grupo8.Models
+{
+    public class Enfermeiros
+    {
+        private int EnfermeiroId{ get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o seu nome" )]
+        private string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o seu numero de telefone")]
+        [RegularExpression(@"9\d{8}", ErrorMessage = "Numero invalido.")]
+        [EmailAddress(ErrorMessage = "Numero Invalido.")]
+        private string Telefone { get; set; }
+
+        [RegularExpression(@"(\w+(\.\w+)*@[a-zA-Z_]+?\.[a-zA-Z]{2,6})",ErrorMessage = "Invalid email.")]
+        [Required(ErrorMessage = "Por favor insira o seu Email")]
+        private string Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o sua morada")]
+        private string Morada { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira a sua profissao ou especializao")]
+        private string Especializacao { get; set; }
+    }
+}
