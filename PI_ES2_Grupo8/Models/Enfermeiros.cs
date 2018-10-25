7565c373730b9438064d2d4b40214695fcc10d8e
Vanderley Quaresma <1012331@sal.ipg.pt>
2018-10-25 15:34:19 +0100

    Modelo para Inserir Enfermeiros

--- PI_ES2_Grupo8/Models/Enfermeiros.cs
+++ PI_ES2_Grupo8/Models/Enfermeiros.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,24 @@ namespace PI_ES2_Grupo8.Models
 {
     public class Enfermeiros
     {
+        public int EnfermeiroId { get; set; }
 
+        [Required(ErrorMessage = "Por favor insira o seu nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o seu numero de telefone")]
+        [RegularExpression(@"(9\d{8})", ErrorMessage = "Numero invalido.")]
+        [EmailAddress(ErrorMessage = "Numero Invalido.")]
+        public string Telefone { get; set; }
+
+        [RegularExpression(@"(\w+(\.\w+)*@[a-zA-Z_]+?\.[a-zA-Z]{2,6})", ErrorMessage = "Invalid email.")]
+        [Required(ErrorMessage = "Por favor insira o seu Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira o sua morada")]
+        public string Morada { get; set; }
+
+        [Required(ErrorMessage = "Por favor insira a sua profissao ou especializao")]
+        public string Especializacao { get; set; }
     }
 }
