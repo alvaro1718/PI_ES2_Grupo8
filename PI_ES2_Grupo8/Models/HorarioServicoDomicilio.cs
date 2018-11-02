7c113cb86564cb286d3931050797245d1dcdd7c2
Nuno Bill Afonso da Silva <1012377@sal.ipg.pt>
2018-11-02 12:06:22 +0000

    h

--- PI_ES2_Grupo8/Models/HorarioServicoDomicilio.cs
+++ PI_ES2_Grupo8/Models/HorarioServicoDomicilio.cs
@@ -8,18 +8,13 @@ namespace PI_ES2_Grupo8.Models
 {
     public class HorarioServicoDomicilio
     {
-            public int HorarioServicoDomicilioId { get; set; }
-            public int Data { get; set; }
-
-            public int Hora { get; set; }
-
-            public string Enfermeiros { get; set; }
-
-            public string Utente { get; set; }
-
-            public string Tratamento { get; set; }
+        public int HorarioServicoDomicilioId { get; set; }
+        public int Data { get; set; }
+        public int Hora { get; set; }
+        public string Enfermeiros { get; set; }
+        public string Utente { get; set; }
+        public string Tratamento { get; set; }
 
 
-        
     }
 }
